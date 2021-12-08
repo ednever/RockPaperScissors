8095e6df8413c77af045e448a97633620730c391
opilane <opilane@HP-CZC6297BBL>
2021-12-08 10:20:37 +0200

    rabotaet 99%

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -10,7 +10,8 @@ while 1:
             print("Жаль :( ")
             quit()
     except ValueError:
-        print("Непрвильный ввод данных")
+        print("Неправильный ввод данных")
+print()
 print("Хочешь сыграть с ботом или другом? (1 - С ботом/0 - С другом) ")
 while 1:
     try:
@@ -20,7 +21,6 @@ while 1:
     except ValueError:
         print("Непрвильный ввод данных")
 a=[1,2,3]
-
 if game==1:
     win_l1=[]
     win1=0
@@ -36,8 +36,10 @@ if game==1:
             except ValueError:
                 print("Непрвильный ввод данных")
         print(f"Ты выбрал {valik}")
+        print()
         shuffle(a)
         print(f"Компутер выбрал {a[0]}")
+        print()
         if a[0]==1 and valik==1 or a[0]==2 and valik==2 or a[0]==3 and valik==3:
             print("Ничья")
         elif a[0]==1 and valik==2 or a[0]==2 and valik==3 or a[0]==3 and valik==1:
@@ -50,6 +52,7 @@ if game==1:
         win2=sum(win_l2)
         print(f"Очки компутера «{win1}»")
         print(f"Очки пользователя «{win2}»")
+        print()
         print("Желаете ли вы очистить счет? (1 - Да/0 - Нет)")
         while 1:
             try:
@@ -75,62 +78,55 @@ if game==1:
             print("Спасибо за игру!")
             print()
             quit()
-
 elif game==0:
-    win_l1=[]
+    print("Пожалуйста, пишите латиницой, иначе программа погибнет!")
+    name1=str(input("Пользователь1, как вас зовут? => "))
+    name2=str(input("Пользователь2, как вас зовут? => "))
+    winl1=[]
     win1=0
-    win_l2=[]
+    winl2=[]
     win2=0
     while 1:
-        print("Пользователь1 выбери: камень - q/ ножницы - w/ бумага - e ")
+        print(f"{name1} выбери: камень - q/ ножницы - w/ бумага - e ")
         if read_key()=="q":
             valik=1
         elif read_key()=="w":
             valik=2
         elif read_key()=="e":
             valik=3
-        print("Пользователь2 выбери: камень - z/ ножницы - x/ бумага - c ")
+        print(f"{name2} выбери: камень - z/ ножницы - x/ бумага - c ")
         if read_key()=="z":
             valik2=1
         elif read_key()=="x":
             valik2=2
         elif read_key()=="c":
             valik2=3
-        print(f"Пользователь1 выбрал {valik}")
-        print(f"Пользователь2 выбрал {valik2}")
+        print(f"{name1} выбрал {valik}")
+        print(f"{name2} выбрал {valik2}")
         if valik==1 and valik2==1 or valik==2 and valik2==2 or valik==3 and valik2==3:
-            print("Ничья")
+            print("У вас получилась ничья")
         elif valik==1 and valik2==2 or valik==2 and valik2==3 or valik==3 and valik2==1:
-            print("Пользователь1 выиграл")
-            win_l1.append(int(1))
+            print(f"{name1} выиграл")
+            winl1.append(int(1))
         elif valik==2 and valik2==1 or valik==3 and valik2==2 or valik==1 and valik2==3:
-            print("Пользователь2 выиграл")
-            win_l2.append(int(1))
-        win1=sum(win_l1)
-        win2=sum(win_l2)
-        print(f"Очки первого пользователя «{win1}»")
-        print(f"Очки второго пользователя «{win2}»")
-        print("Желаете ли вы очистить счет? (1 - Да/0 - Нет)")
-        while 1:
-            try:
-                point=int(input(" => "))
-                if point==1 or point==0:
-                    break
-            except ValueError:
-                print("Непрвильный ввод данных")
-        if point==1:
-            win_l1.clear()
-            win_l2.clear()    
-        print("Желаете ещё одну игру? (1 - Да/0 - Нет)")
-        while 1:
-            try:
-                zxc=int(input(" => "))
-                if zxc==1 or zxc==0:
-                    break
-            except ValueError:
-                print("Непрвильный ввод данных")
-        if zxc==1:
+            print(f"{name2} выиграл")
+            winl2.append(int(1))
+        win1=sum(winl1)
+        win2=sum(winl2)
+        print(f"{name1} всего набрал «{win1}»")
+        print(f"{name2} всего набрал «{win2}»")
+        print()
+        print("Желаете ли вы очистить счет? (backspace - Да/space - Нет)")
+        print("При создании игры первый игрок всегда проигрывал, поэтому у него не очищается счёт при выигрышах")
+        if read_key() == "backspace":
+            winl1.clear()
+            winl2.clear()
+        if read_key() == "space":
+            print("Так держать!")
+        print()
+        print("Желаете сыграть ещё одну игру? (r - Да/t - Нет)")
+        if read_key() == "r":                   
             continue
-        else: 
+        elif read_key() == "t": 
             print("Спасибо за игру!")
             quit()
